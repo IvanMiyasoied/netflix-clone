0e3e12ee3134bb4c2cdb24e5f7475675da3e1d81
IvanMiyasoied <121316974+IvanMiyasoied@users.noreply.github.com>
2023-10-29 00:35:16 +0300

    Auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
+import { AuthContextProvider } from "./context/AuthContext"
 
 
 function App() {
@@ -10,10 +11,15 @@ function App() {
 
   return (
     <>
+      <AuthContextProvider>
+
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+
+      </AuthContextProvider>
+
     </>
   )
 }
